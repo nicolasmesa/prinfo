c2fc4b685703f55502340e412fa5f183047aecd9
Di Ruan <dr2763@columbia.edu>
2014-09-27 18:14:02 -0400

    checkpatch for prinfo.c

--- prinfo.c
+++ prinfo.c
@@ -2,26 +2,27 @@
 #include <stdio.h>
 #include <sys/syscall.h>
 #include <stdlib.h>
+
 #define NR 3000
 
 struct prinfo {
-        pid_t parent_pid;               /* process id of parent */
-        pid_t pid;                      /* process id */
-        pid_t first_child_pid;          /* pid of youngest child */
-        pid_t next_sibling_pid;         /* pid of older sibling */
-        long state;                     /* current state of process */
-        long uid;                       /* user id of process owner */
-        char comm[64];                  /* name of program executed */
+	pid_t parent_pid;               /* process id of parent */
+	pid_t pid;                      /* process id */
+	pid_t first_child_pid;          /* pid of youngest child */
+	pid_t next_sibling_pid;         /* pid of older sibling */
+	long state;                     /* current state of process */
+	long uid;                       /* user id of process owner */
+	char comm[64];                  /* name of program executed */
 };
 
-int ptree(struct prinfo* buf, int* nr)
+int ptree(struct prinfo *buf, int *nr)
 {
 	return syscall(223, buf, nr);
 }
 
 void print_tabs(int count)
 {
-	while(count--)
+	while (count--)
 		printf("\t");
 }
 
@@ -34,8 +35,9 @@ void print_tree(struct prinfo *buff, int nr)
 	prev_parent = -1;
 	prev_child = 0;
 
-	printf("%s,%d,%ld,%d,%d,%d,%lu\n", buff[0].comm, buff[0].pid, buff[0].state,
-                                buff[0].parent_pid, buff[0].first_child_pid, buff[0].next_sibling_pid, buff[0].uid);
+	printf("%s,%d,%ld,%d,%d,%d,%lu\n", buff[0].comm, buff[0].pid,
+		buff[0].state, buff[0].parent_pid,
+		buff[0].first_child_pid, buff[0].next_sibling_pid, buff[0].uid);
 
 	for (i = 1; i < nr; i++) {
 		curr_parent = buff[i].parent_pid;
@@ -43,8 +45,7 @@ void print_tree(struct prinfo *buff, int nr)
 		if (curr_parent == prev_child) {
 			curr_level++;
 			levels[curr_level] = buff[i].pid;
-			
-		}else if (curr_parent != prev_parent) {
+		} else if (curr_parent != prev_parent) {
 			while (levels[curr_level] != curr_parent)
 				curr_level--;
 
@@ -56,25 +57,25 @@ void print_tree(struct prinfo *buff, int nr)
 		prev_parent = curr_parent;
 
 		print_tabs(curr_level);
-		printf("%s,%d,%ld,%d,%d,%d,%lu\n", buff[i].comm, buff[i].pid, buff[0].state,
-				buff[i].parent_pid, buff[i].first_child_pid, buff[i].next_sibling_pid, buff[i].uid);
+		printf("%s,%d,%ld,%d,%d,%d,%lu\n", buff[i].comm, buff[i].pid,
+			buff[0].state, buff[i].parent_pid,
+			buff[i].first_child_pid,
+			buff[i].next_sibling_pid, buff[i].uid);
 	}
 }
 
-int main(int argc, char **argv) {
-	
-	if(argc != 2) {
-	
-	printf("Usage: ./prinfo <num of processes requested>\n");
-	return 0;	
-	
-	}	
-	
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		printf("Usage: ./prinfo <num of processes requested>\n");
+		return 0;
+	}
+
 	int nr = atoi(argv[1]);
 	struct prinfo buf[nr];
 	int ret;
 
-	ret = ptree( buf, &nr);	
+	ret = ptree(buf, &nr);
 	printf("Returned: %d\n", ret);
 
 	ret = ret < nr ? ret : nr;
